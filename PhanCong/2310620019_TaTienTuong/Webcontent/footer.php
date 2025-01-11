e3c684f044d56224e5dca168f3c962c6e53629d6
tatuong <tatuong16@gmail.com>
2025-01-11 15:36:00 +0700

    update file 2310620019_TaTienTuong

---
+++ PhanCong/2310620019_TaTienTuong/Webcontent/footer.php
@@ -0,0 +1,25 @@
+<!-- ##### Footer Area Start ##### -->
+<footer class="footer-area">
+    <!-- Footer Logo -->
+    <div class="footer-logo mb-100">
+        <a href="index.php"><img src="img/core-img/hpc.png" alt=""></a>
+    </div>
+    <!-- Footer Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="footer-content text-center">
+                    <!-- Social Info -->
+                    <div class="footer-social-info">
+                        <a href="https://www.facebook.com/Caodangcongnghebachkhoahanoi/photos/" data-toggle="tooltip" data-placement="top" title="Facebook">Facebook</a>
+                        <a href="https://bachkhoahanoi.edu.vn/" data-toggle="tooltip" data-placement="top" title="Cao Đẳng Công Nghệ Bách Khoa Hà Nội">HPC</a>
+                        <a href="https://bachkhoahanoi.edu.vn/category/khoa-cong-nghe-thong-tin/" data-toggle="tooltip" data-placement="top" title="Khoa công nghệ thông tin">IT</a>
+                    </div>
+
+                    <p class="mb-15">Khoa công nghệ thông tin - Cao Đẳng Công Nghệ Bách Khoa Hà Nội</p>
+                </div>
+            </div>
+            </sdiv>
+        </div>
+</footer>
+<!-- ##### Footer Area Start ##### -->
