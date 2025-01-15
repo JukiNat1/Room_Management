32e0a30d1cdc1753dabd0ecda103794ceaf8e5de
Phạm Anh Đông <phamanhdong99@gmail.com>
2025-01-15 20:55:33 +0700

    Update KhuVuc.php

--- PhanCong/2309620125_PhamAnhDong/Model/KhuVuc.php
+++ PhanCong/2309620125_PhamAnhDong/Model/KhuVuc.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of KhuVuc
- *
- * @author tung
- */
 class KhuVuc
 {
     var $MaKhuVuc;
@@ -23,5 +12,4 @@ class KhuVuc
         $this->TenQuanHuyen = $TenQuanHuyen;
     }
 
-    //put your code here
 }
