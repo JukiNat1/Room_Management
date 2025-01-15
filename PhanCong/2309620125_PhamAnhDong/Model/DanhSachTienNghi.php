8b4cf2f4c7f7fb91daa7dfada24f760ca094a9ba
Phạm Anh Đông <phamanhdong99@gmail.com>
2025-01-15 20:54:48 +0700

    Update DanhSachTienNghi.php

--- PhanCong/2309620125_PhamAnhDong/Model/DanhSachTienNghi.php
+++ PhanCong/2309620125_PhamAnhDong/Model/DanhSachTienNghi.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of DanhSachTienNghi
- *
- * @author tung
- */
 class DanhSachTienNghi
 {
     var $MaTienNghi;
@@ -21,5 +10,4 @@ class DanhSachTienNghi
         $this->MaPhong = $MaPhong;
     }
 
-    //put your code here
 }
